60ff1e3993bbd6a4f57d2d7eeaca14888315fc95
Vasliy-Vasily <orvashka205@gmail.com>
2026-09-04 02:48:51 +0300

    Update data_input.py

--- data_input.py
+++ data_input.py
@@ -4,36 +4,29 @@ import pandas as pd
 
 def get_data(args): #Get data from the user
     filename = args.file
-
     if not os.path.exists(filename):
         raise FileNotFoundError(
-            f"Файл '{filename}' не найден."
+            f"File '{filename}' is not found!"
         )
-
     try:
         data = pd.read_csv(filename)
     except pd.errors.EmptyDataError:
-        raise ValueError("CSV-файл пуст.")
+        raise ValueError("Error: file is empty!")
     except pd.errors.ParserError:
-        raise ValueError("Неверный формат CSV-файла.")
-
+        raise ValueError("Error: invalid format for CVS file!")
     data = data.set_index(data.columns[0])
-
     number_of_groups = len(data)
     number_of_data = len(data.columns)
-
     if args.type == "categorical":
         k = 1
-
-        data["Итого"] = data.sum(axis=1)
-        data.loc["Итого"] = data.sum(axis=0)
-        data.loc["Итого", "Итого"] = 0
-
+        data = data.apply(pd.to_numeric)
+        data["Total"] = data.sum(axis=1)
+        data.loc["Total"] = data.sum(axis=0)
+        data.loc["Total", "Total"] = 0
     else:
         k = 2
-
-    print("\nВаши данные:")
+        data = data.apply(pd.to_numeric)
+    print("Your data:")
     print(data.to_string())
     print()
-
     return data, number_of_groups, number_of_data, k
