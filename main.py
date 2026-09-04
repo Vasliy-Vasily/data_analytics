2be65c440487e79edbf79f7e912fbb6503e103ee
Vasliy-Vasily <orvashka205@gmail.com>
2026-09-04 17:21:13 +0300

    Update main.py
    Changed the unit test logic: the user now analyzes the data separately and runs the unit tests separately, if needed

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from unit_tests import unit_test_result
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Statistical analysis of an A/B test")
-    parser.add_argument("--file", type=str, required=True, help="Path to the CSV file")
-    parser.add_argument("--type", choices=["categorical", "quantitative"], required=True, help="Data type")
+    parser.add_argument("--file", type=str, help="Path to the CSV file")
+    parser.add_argument("--type", choices=["categorical", "quantitative"], help="Data type")
     parser.add_argument("--column", type=int, choices=[1, 2], help="Data category number for the Z-test" )
     parser.add_argument("--alternative", choices=["one-sided", "two-sided"], help="Hypothesis type for the Z-test" )
     parser.add_argument("--unit-test", action="store_true", help="Run unit tests"  )
@@ -17,9 +17,11 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     if args.unit_test:
-        if not unit_test_result():
-            print("Program cannot run because there are errors.")
-            return
+        exit(0 if unit_test_result() else 1)
+    if not args.file or not args.type:
+        print("Error: --file and --type are required for data analysis.")
+        print("Use --help for more information.")
+        return
     print("STATISTICAL ANALYSIS OF AN A/B TEST")
     print()
     df, number_of_groups, number_of_data, k = get_data(args)
